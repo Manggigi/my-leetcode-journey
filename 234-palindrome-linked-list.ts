c73517439c6e550b30feafe0738bf796247406f7
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-10-17 23:56:34 +0800

    solve: 234 palindrome linked list

---
+++ 234-palindrome-linked-list.ts
@@ -0,0 +1,42 @@
+// Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+  let arr: number[] = [];
+  let current = head;
+  while (current !== null) {
+    arr.push(current.val);
+    current = current.next;
+  }
+  for (let i = 0; i < arr.length / 2; i++) {
+    if (arr[i] !== arr[arr.length - 1 - i]) {
+      return false;
+    }
+  }
+  // let counter = 0;
+  // while (counter < arr.length / 2) {
+  //   if (arr[counter] !== arr[arr.length - 1 - counter]) {
+  //     return false;
+  //   }
+  //   counter++;
+  // }
+  return true;
+}
+
+// test
+const node1 = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(2);
+const node4 = new ListNode(1);
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+console.log(isPalindrome(node1)); // true
+console.log(isPalindrome(node2)); // false
